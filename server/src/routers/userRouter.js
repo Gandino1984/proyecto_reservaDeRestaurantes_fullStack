117adcd54759de30e83acb2337d4b34dda6c009f
Asier <asierherrera97@gmail.com>
2024-06-11 12:41:54 +0200

    Ahora si

--- server/src/routers/userRouter.js
+++ server/src/routers/userRouter.js
@@ -7,7 +7,6 @@ const router  = Router();
 router.get("/",userApiController.getAll);
 //router.get("/byproperty",userApiController.getByProperty);
 router.get("/:id",userApiController.getById);
-router.post("/",userApiController.create);
 router.put("/:id",userApiController.update);
 router.delete("/:id",userApiController.remove);
 
