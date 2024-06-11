8753b2dadf74286067e04c2521f14114c82266b1
Gandino1984 <andinogerman@gmail.com>
2024-06-11 10:47:42 +0200

    images

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
+import React from 'react'
 import './App.css'
 import Navbar from './navbar/Navbar.jsx'
+import {useNavigate, Link, BrowserRouter} from 'react-router-dom'
+
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-    </>
+    </BrowserRouter>
   )
 }
 
