d979390bf87befe2befc998a10f1a743f04385c2
Asier <asierherrera97@gmail.com>
2024-06-11 17:02:25 +0200

    Rutas, modelo y controladores para mesas

---
+++ server/src/controllers/mesas/mesasApiController.js
@@ -0,0 +1,50 @@
+import mesasController from "./mesasController.js";
+
+const getAll = async(req,res)=>{
+    const mesasData = req.session.user
+    console.log("LA mesasDATA ES:", mesasData)
+    const esAdmin = req.session.user.esAdmin
+    console.log("ESADMIN ES IGUAL A:",esAdmin)
+    const {error,data} = await mesasController.getAll(mesasData);
+    res.json({error,data});
+}
+
+const getByProperty = async(req,res)=>{
+    const {property,value} = req.query;
+    const mesas = await mesasController.getByProperty(property,value);
+    res.json({data:mesas})
+}
+
+const getById = async (req,res) =>{
+    const id = req.params.id
+    const mesas = await mesasController.getById(id);
+    res.json({data:mesas});
+}
+
+const update = async(req,res)=>{
+    const id = req.params.id;
+    const mesas = await mesasController.update(id,req.body);
+    res.json({data:mesas})
+}
+
+const remove = async(req,res)=>{
+    const id = req.params.id;
+    const mesas = await mesasController.remove(id);
+    res.json({data:mesas})
+}
+
+const create = async(req,res)=>{
+    const sesionUserId = req.session.user.user_id
+    const mesa = await mesasController.create(req.body, sesionUserId);
+    res.json({data:mesa})
+}
+
+export default{
+    getAll,
+    getById,
+    getByProperty,
+    update,
+    create,
+    remove
+}
+
