3f8785b87735c06ad5a040f63d4a2afd7a78d915
Gandino1984 <andinogerman@gmail.com>
2024-06-11 12:48:10 +0200

    home image

--- client/src/home/Landing.jsx
+++ client/src/home/Landing.jsx
@@ -3,12 +3,15 @@ import bg1 from '../images/bgHome.jpg'
 
 function Landing() {
   return (
-    <div className='containerLanding'>
-      <h1>Bookit</h1>
-      <div className='background' style={{backgroundImage: `url(${bg1})`}}>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus vitae repellat. Odit excepturi quo numquam ea ad explicabo incidunt expedita autem pariatur, corrupti repudiandae nobis amet in repellendus minus.</p>
-      </div>
-    </div>
+    <>
+        <div className='containerLanding'>
+        <h1>Bookit</h1>
+        <div className='background' style={{backgroundImage: `url(${bg1})`}}>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam ducimus vitae repellat. Odit excepturi quo numquam ea ad explicabo incidunt expedita autem pariatur, corrupti repudiandae nobis amet in repellendus minus.</p>
+        </div>
+        </div>
+    </>
+    
   )
 }
 
