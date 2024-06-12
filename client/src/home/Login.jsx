4052b473e7e500442a65cc1bebeb829ef5c07108
Gandino1984 <andinogerman@gmail.com>
2024-06-12 16:46:20 +0200

    login user action menu

--- client/src/home/Login.jsx
+++ client/src/home/Login.jsx
@@ -15,7 +15,7 @@ function Login({userType}) {
   const [restaurantIsLogin, setrestaurantIsLogin] = useState(false);
   const [restaurantIsRegister, setrestaurantIsregister] = useState(false);
 
-  useEffect(() => {
+  useEffect((user, userAction) => {
     if (user === "client") {
       setrestaurantIsLogin(false);
       setrestaurantIsregister(false);
@@ -28,16 +28,27 @@ function Login({userType}) {
     }
   }, [user, clientIsLogin, clientIsRegister, restaurantIsLogin, restaurantIsRegister])
 
+  // radioOptionHandler
+
   return (
     <div className={styles.containerLogin}>
                   <form className={styles.formContainer} action="">
-                          <h2>Iniciar Sesión:</h2>
-                            {clientIsLogin && <input type="text" id="clientName" name="clientName" placeholder="Nombre de cliente" />}
-                            {clientIsLogin && <input type="password" id="clientPasssword" name="clientPasssword" placeholder="Contraseña de cliente" />}
-                            {clientIsRegister && <input type="password" id="repeatClientPasssword" name="clientRepeatPasssword" placeholder="Repetir contraseña de cliente" />}
-                            {restaurantIsLogin && <input type="text" id="restaurantName" name="restaurantName" placeholder="Nombre de restaurante" />}
-                            {restaurantIsLogin && <input type="password" id="restaurantPasssword" name="restaurantPasssword" placeholder="Contraseña de restaurante" />}
-                            {restaurantIsRegister && <input type="password" id="restaurantRepeatPasssword" name="restaurantRepeatPasssword" placeholder="Repetir contraseña de restaurante" />}
+                          <div className={styles.menuContainer}>
+                                {userAction === "login" && <h2>Iniciar Sesión:</h2>}
+                                {userAction === "register" && <h2>Registrar Cuenta:</h2>}
+                                <div className={styles.radios} onChange={e=>setuserAction(e.target.value)}>
+                                    <input className={styles.radioOption} id="loginOption" name='userAction' type="radio" value="login" />
+                                    <input className={styles.radioOption} id="registerOption" name='userAction' type="radio" value="register" />
+                                </div>
+                          </div>
+                          
+                          {clientIsLogin && <input type="text" id="clientName" name="clientName" placeholder="Nombre de cliente" />}
+                          {clientIsLogin && <input type="password" id="clientPasssword" name="clientPasssword" placeholder="Contraseña de cliente" />}
+                          {clientIsRegister && <input type="password" id="repeatClientPasssword" name="clientRepeatPasssword" placeholder="Repetir contraseña de cliente" />}
+                          {restaurantIsLogin && <input type="text" id="restaurantName" name="restaurantName" placeholder="Nombre de restaurante" />}
+                          {restaurantIsLogin && <input type="password" id="restaurantPasssword" name="restaurantPasssword" placeholder="Contraseña de restaurante" />}
+                          {restaurantIsRegister && <input type="password" id="restaurantRepeatPasssword" name="restaurantRepeatPasssword" placeholder="Repetir contraseña de restaurante" />}
+                          
                           <button className={styles.btn1}>ENTRAR</button>
                   </form>
     </div>
