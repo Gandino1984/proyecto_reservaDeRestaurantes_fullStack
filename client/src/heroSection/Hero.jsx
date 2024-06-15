38c7816442efea2bec43e8e5fb6c63e5a242bdc4
Gandino1984 <andinogerman@gmail.com>
2024-06-15 17:33:55 +0200

    cambios landing

--- client/src/heroSection/Hero.jsx
+++ client/src/heroSection/Hero.jsx
@@ -1,18 +1,17 @@
 import styles from './Hero.module.css'
 
-function Hero() {
+function Hero({heroBtnClick}) {
     return (
         <div className={styles.container}>
                 <div className={styles.hero}>
-                        <div className={styles.text1}>
-                            <p>Dinos cuánta <br />hambre tienes</p>
-                        </div>
-                        <div className={styles.text2}>
-                            <p className={styles.text3}> Book-IT programa <br /> tu solución</p>
-                            <div className={styles.btnContainer}>
-                                <button className={styles.btn}>RESERVA YA</button>
+                            <div className={styles.sub}>
+                                <p className={styles.text1}>Dinos cuánta <br />hambre tienes</p>
+                                <button className={styles.btn} onClick={e=>heroBtnClick(e)}>RESERVA UNA MESA</button>
                             </div>
-                        </div>
+                            {/* <div className={styles.sub}>
+                                <p className={styles.text1}>O qué tipo de comida <br />puedes ofrecer</p>
+                                <button className={styles.btn} onClick={e=>heroBtnClick(e)}>REGISTRA TU RESTAURANTE</button>
+                            </div>       */}
                 </div>
               
         </div>
--- client/src/navbar/Navbar.jsx
+++ client/src/navbar/Navbar.jsx
@@ -3,46 +3,16 @@ import styles from './Navbar.module.css'
 import { useState } from 'react'
 import RestaurantSearchBar from './RestaurantSearchBar';
 
-function Navbar({handleUserTypeSelection, loginFormOpen, onCloseRestaurantCard}) {
-
-  const [selectorState, setselectorState]  = useState("activeSelector");
-  
-  function onChangeRadiohandler(e){
-    handleUserTypeSelection(e);
-    setselectorState("inactiveSelector") 
-  }
-
-  useEffect(() => {
-    if(loginFormOpen === true){
-      setselectorState("inactiveSelector") 
-    }else{
-      setselectorState("activeSelector")
-    }
-  },[loginFormOpen])
-   
-  function onCloseRestaurantCardHandler(e){
-    onCloseRestaurantCard();
-  }
+function Navbar() {
 
   return (
     <>
         <div className={styles.containerNavbar}>
               <div className={styles.containerTypewriter}>
-                    <p className={styles.typewriter}>Book-IT  !!</p>
-              </div>
-        
-              <div className={`${styles.radioBtnsContainer} ${styles[selectorState]}`}>
-                    <div className={styles.labels}>
-                        <label  htmlFor="clientOption">CLIENTE</label>
-                        <label  htmlFor="restaurantOption">RESTAURANTE</label>
-                    </div>                   
-                    <div className={styles.radios} onChange={e=>onChangeRadiohandler(e)}>
-                        <input className={styles.radioOption} value="client"  id="clientOption" name='userType' type="radio" />
-                        <input className={styles.radioOption} value="restaurant" id="restaurantOption" name='userType' type="radio" />
-                    </div>   
+                    <p className={styles.typewriter}>Book-IT</p>
               </div>
 
-              <RestaurantSearchBar onCloseRestaurantCard={onCloseRestaurantCard}/>
+              <RestaurantSearchBar />
 
         </div>
     </>
