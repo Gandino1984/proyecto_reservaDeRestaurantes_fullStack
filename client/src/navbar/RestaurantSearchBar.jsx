0cdd6f600c505f15d6054b8637e7f5f2aef3e0a6
Gandino1984 <andinogerman@gmail.com>
2024-06-15 08:50:10 +0200

    css

--- client/src/navbar/RestaurantSearchBar.jsx
+++ client/src/navbar/RestaurantSearchBar.jsx
@@ -5,7 +5,7 @@ function RestaurantSearchBar() {
   return (
     <div className={styles.container}>
         <label className={styles.labelSearch} htmlFor="restaurantSearch"><ion-icon name="search"></ion-icon></label>
-        <input type="text" name="restaurantSearch" placeholder='Buscar un restaurante'/>
+        <input className={styles.inputSearch} type="text" name="restaurantSearch" placeholder='Buscar un restaurante'/>
     </div>
   )
 }
