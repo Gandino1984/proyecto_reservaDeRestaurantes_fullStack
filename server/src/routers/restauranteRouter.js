35433fe2041f959a922503326807652effe99128
Gandino1984 <andinogerman@gmail.com>
2024-06-19 18:48:00 +0200

    mostrar restaurantes

--- server/src/routers/restauranteRouter.js
+++ server/src/routers/restauranteRouter.js
@@ -6,15 +6,13 @@ import restauranteApiController from "../controllers/restaurante/restauranteApiC
 const router  = Router();
 
 router.get("/",restauranteApiController.getAll);
-router.get("/user", isAuthenticated,restauranteApiController.getRestaurantesByUserId);
+router.get("/user",isAuthenticated, restauranteApiController.getRestaurantesByUserId);
 router.get("/tipo/:tipo",restauranteApiController.getRestauranteByTipo);
 router.get("/byproperty",restauranteApiController.getByProperty);
 router.get("/busqueda/:busquedaData",restauranteApiController.barraDeBusqueda)
-router.get("/:id", restauranteApiController.getById);
-router.put("/:id", isAuthenticated, restauranteApiController.updateRestaurante);
+router.get("/:id",restauranteApiController.getById);
+router.put("/:id",isAuthenticated, restauranteApiController.updateRestaurante);
 router.delete("/:id", isAuthenticated, restauranteApiController.remove);
 router.post("/",isAuthenticated, restauranteApiController.create);
-
-
 
 export default router;
