b708020815b2de209fbf852f7b05e754297bab2a
Gandino1984 <andinogerman@gmail.com>
2024-06-22 21:15:40 +0200

    css horizontal scroll

--- client/src/components/mostrarRestaurantes/ShowRestaurants.jsx
+++ client/src/components/mostrarRestaurantes/ShowRestaurants.jsx
@@ -1,67 +1,67 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import GeneralContext from '../../context/GeneralContext';
-import { getAllRestaurantes } from '../../utils/restauranteFetch'; 
-import './ShowRestaurants.css'
-import { useContext } from 'react';
+import { getAllRestaurantes } from '../../utils/restauranteFetch';
+import './ShowRestaurants.css';
 
 function ShowRestaurants() {
+  const { restaurantData, setrestaurantData, arrayRestaurantData, setarrayRestaurantData } = useContext(GeneralContext);
+  const scrollContainerRef = useRef(null);
 
-  const { restaurantData,
-          setrestaurantData,
-          arrayRestaurantData,
-          setarrayRestaurantData
-        } = useContext(GeneralContext);
+  console.log('ShowRestaurants, Context: restaurantData= ', restaurantData);
 
-        console.log('ShowRestaurants, Context: restaurantData= ', restaurantData)
-
-  async function searchRestaurants(){
-    const response = await getAllRestaurantes();
-    setrestaurantData(response.data);
-    console.log("response.data= ", response.data) ;
+  async function searchRestaurants() {
+    try {
+      const response = await getAllRestaurantes();
+      setrestaurantData(response.data);
+      console.log("response.data= ", response.data);
+    } catch (error) {
+      console.error('Failed to fetch restaurants:', error);
+    }
   }
-  
+
   useEffect(() => {
     searchRestaurants();
   }, []);
 
-  function mappedArrayhandler(array){
-    setarrayRestaurantData(array);
-  }
-
-  useEffect(
-    () => {
-      if (restaurantData) {
-        const arrayMapped = restaurantData.map(restaurant => 
-          <li key={restaurant.Restaurante_id} className='card'>
-            <h3>{restaurant.Name}</h3>
-            <div className='cardSchedule'>
-                <h5>{restaurant.Hora_Apertura}</h5>
-                <h5>{restaurant.Hora_Cierre}</h5>
-            </div>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum doloremque magnam tenetur hic quisquam iure quibusdam eos eveniet aliquid iste officia non quas.</p>
-          </li>
-        )
-        mappedArrayhandler(arrayMapped)
-        
-      } 
-  }
-  , [restaurantData]
-);
-
-
-  
-  return (
-    <div className='containerShowRestaurants'>
-        <div className='showRestaurants'>
-          <div className='cardGroup'>
-              <ul>  
-                      {arrayRestaurantData}
-              </ul>
+  useEffect(() => {
+    if (restaurantData) {
+      const arrayMapped = restaurantData.map(restaurant => (
+        <li key={restaurant.Restaurante_id} className='card'>
+          <h3>{restaurant.Name}</h3>
+          <div className='cardSchedule'>
+            <h5>{restaurant.Hora_Apertura}</h5>
+            <h5>{restaurant.Hora_Cierre}</h5>
           </div>
-        </div>
-    </div>
-  )
+        </li>
+      ));
+      
+      setarrayRestaurantData(arrayMapped);
+    }
+  }, [restaurantData, setarrayRestaurantData]);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <>
+      <button className='scrollButton left' onClick={scrollLeft}><ion-icon name="chevron-back-outline"></ion-icon></button>
+      <div className='showRestaurants' ref={scrollContainerRef} >
+        <ul className='cardGroup' >
+          {arrayRestaurantData}
+        </ul>
+      </div>
+      <button className='scrollButton right' onClick={scrollRight}><ion-icon name="chevron-forward-outline"></ion-icon></button>
+    </>
+  );
 }
 
-export default ShowRestaurants
+export default ShowRestaurants;
